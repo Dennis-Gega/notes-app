3f997eb64c89a28bd1d19a6988d61dd6811726cb
dennis gega <dennisgega@denniss-MacBook-Air.local>
2025-01-07 14:19:34 -0500

    added unit tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState, useEffect } from 'react'
-import noteService from './services/notes'
+import { useState, useEffect, useRef } from 'react'
 import Note from './components/Note'
 import Notification from './components/Notification'
-import Footer from "./components/Footer.jsx";
+import Footer from './components/Footer'
+import noteService from './services/notes'
+import loginService from './services/login'
+import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
+import NoteForm from './components/NoteForm'
 
 const App = () => {
     const [notes, setNotes] = useState([])
-    const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
-    const [errorMessage, setErrorMessage] = useState('')
+    const [errorMessage, setErrorMessage] = useState(null)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
+    const [loginVisible, setLoginVisible] = useState(false)
+
+    const noteFormRef = useRef()
+
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+            noteService.setToken(user.token)
+        }
+    }, [])
 
     useEffect(() => {
         noteService
@@ -18,7 +36,6 @@ const App = () => {
             })
     }, [])
 
-
     const toggleImportanceOf = id => {
         const note = notes.find(n => n.id === id)
         const changedNote = { ...note, important: !note.important }
@@ -26,7 +43,7 @@ const App = () => {
         noteService
             .update(id, changedNote)
             .then(returnedNote => {
-                setNotes(notes.map(note => note.id === id ? returnedNote : note))
+                setNotes(notes.map(note => note.id !== id ? note : returnedNote))
             })
             .catch(error => {
                 setErrorMessage(
@@ -35,40 +52,86 @@ const App = () => {
                 setTimeout(() => {
                     setErrorMessage(null)
                 }, 5000)
-                setNotes(notes.filter(n => n.id !== id))
             })
     }
 
-    const addNote = (event) => {
+    const handleLogin = async (event) => {
         event.preventDefault()
-        const noteObject = {
-            content: newNote,
-            important: Math.random() > 0.5
-        }
 
+        try {
+            const user = await loginService.login({
+                username, password,
+            })
+            window.localStorage.setItem(
+                'loggedNoteappUser', JSON.stringify(user)
+            )
+            noteService.setToken(user.token)
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage('wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+
+    const addNote = (noteObject) => {
+        noteFormRef.current.toggleVisibility()
         noteService
             .create(noteObject)
             .then(returnedNote => {
                 setNotes(notes.concat(returnedNote))
-                setNewNote('')
             })
-    }
-
-    const handleNoteChange = (event) => {
-        setNewNote(event.target.value)
     }
 
     const notesToShow = showAll
         ? notes
-        : notes.filter(note => note.important === true)
+        : notes.filter(note => note.important)
+
+    const loginForm = () => {
+        const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+        const showWhenVisible = { display: loginVisible ? '' : 'none' }
+
+        return (
+            <div>
+                <div style={hideWhenVisible}>
+                    <button onClick={() => setLoginVisible(true)}>log in</button>
+                </div>
+                <div style={showWhenVisible}>
+                    <LoginForm
+                        username={username}
+                        password={password}
+                        handleUsernameChange={({ target }) => setUsername(target.value)}
+                        handlePasswordChange={({ target }) => setPassword(target.value)}
+                        handleSubmit={handleLogin}
+                    />
+                    <button onClick={() => setLoginVisible(false)}>cancel</button>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div>
             <h1>Notes</h1>
-            {errorMessage && <Notification message={errorMessage} />}
+            <Notification message={errorMessage} />
+
+            {!user && loginForm()}
+            {user && <div>
+                <p>{user.name} logged in</p>
+                <Togglable buttonLabel='new note' ref={noteFormRef}>
+                    <NoteForm
+                        createNote={addNote}
+                    />
+                </Togglable>
+            </div>
+            }
+
             <div>
                 <button onClick={() => setShowAll(!showAll)}>
-                    show {showAll ? 'important' : 'all'}
+                    show {showAll ? 'important' : 'all' }
                 </button>
             </div>
             <ul>
@@ -80,13 +143,6 @@ const App = () => {
                     />
                 )}
             </ul>
-            <form onSubmit={addNote}>
-                <input
-                    value={newNote}
-                    onChange={handleNoteChange}
-                />
-                <button type="submit">save</button>
-            </form>
             <Footer />
         </div>
     )
--- server/mongo.js
+++ server/mongo.js
@@ -1,42 +1,40 @@
-//IH2Mbh04DFG8QhE6
-
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://dennisgega:${password}@cluster0.rd2cz.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
-
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
+mongoose.set('strictQuery', false)
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
     content: String,
     important: Boolean,
-})
+  })
 
-const Note = mongoose.model('Note', noteSchema)
+  const Note = mongoose.model('Note', noteSchema)
 
-/*
-const note = new Note({
-    content: 'HTML is easy',
+  /*
+  const note = new Note({
+    content: 'HTML is x',
     important: true,
-})
+  })
 
-note.save().then(result => {
+  note.save().then(result => {
     console.log('note saved!')
     mongoose.connection.close()
-})
- */
-Note.find({}).then(result => {
+  })
+  */
+  Note.find({}).then(result => {
     result.forEach(note => {
-        console.log(note)
+      console.log(note)
     })
     mongoose.connection.close()
+  })
 })
+
